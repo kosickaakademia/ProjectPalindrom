edb8fac3238dc97689c4c522c8198544b4c43cad
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2020-10-08 13:29:48 +0200

    added new function hello()

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 package sk.kosickaakademia.onofrej.projectmath;
 
+// Roland
 public class Main {
 
     public static void main(String[] args) {
-        Square stvorec1 = new Square(3);
+        Square stvorec1 = new Square(10);
         int result = stvorec1.calcPerimeter();
         System.out.println("Result: "+result);
     }
+
+public void hello(){
+
+}
 //test
 }
